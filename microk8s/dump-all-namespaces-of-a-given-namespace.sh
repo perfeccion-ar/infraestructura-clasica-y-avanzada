bbc581c3a7acecbee0cc5d097278e90e819dfd61
Sergio Alonso <sergio.alonso@sensar.nl>
2025-08-10 18:27:42 -0300

    some fixes

---
+++ microk8s/dump-all-namespaces-of-a-given-namespace.sh
@@ -0,0 +1,47 @@
+#/bin/bash
+
+NAMESPACE="test-environment"
+OUTPUT_FILE="${NAMESPACE}_all_manifests.yaml"
+
+echo
+echo "DUMPING $NAMESPACE"
+echo
+echo "# pods" > "$OUTPUT_FILE"
+kubectl get pods -n "$NAMESPACE" -o yaml >> "$OUTPUT_FILE"
+echo --- >> "$OUTPUT_FILE"
+
+echo "# deployments" >> "$OUTPUT_FILE"
+kubectl get deployments -n "$NAMESPACE" -o yaml >> "$OUTPUT_FILE"
+echo --- >> "$OUTPUT_FILE"
+
+echo "# services" >> "$OUTPUT_FILE"
+kubectl get services -n "$NAMESPACE" -o yaml >> "$OUTPUT_FILE"
+echo --- >> "$OUTPUT_FILE"
+
+echo "# ingress" >> "$OUTPUT_FILE"
+kubectl get ingress -n "$NAMESPACE" -o yaml >> "$OUTPUT_FILE"
+echo --- >> "$OUTPUT_FILE"
+
+echo "# replicaset" >> "$OUTPUT_FILE"
+kubectl get replicaset -n "$NAMESPACE" -o yaml >> "$OUTPUT_FILE"
+echo --- >> "$OUTPUT_FILE"
+
+echo "# secrets" >> "$OUTPUT_FILE"
+kubectl get secrets -n "$NAMESPACE" -o yaml >> "$OUTPUT_FILE"
+echo --- >> "$OUTPUT_FILE"
+
+echo "# configmaps" >> "$OUTPUT_FILE"
+kubectl get configmaps -n "$NAMESPACE" -o yaml >> "$OUTPUT_FILE"
+echo --- >> "$OUTPUT_FILE"
+
+echo "# daemonsets" >> "$OUTPUT_FILE"
+kubectl get daemonsets -n "$NAMESPACE" -o yaml >> "$OUTPUT_FILE"
+echo --- >> "$OUTPUT_FILE"
+
+echo "# cronjobs" >> "$OUTPUT_FILE"
+kubectl get cronjobs -n "$NAMESPACE" -o yaml >> "$OUTPUT_FILE"
+echo --- >> "$OUTPUT_FILE"
+
+echo "# jobs" >> "$OUTPUT_FILE"
+kubectl get jobs -n "$NAMESPACE" -o yaml >> "$OUTPUT_FILE"
+echo --- >> "$OUTPUT_FILE"
